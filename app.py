34036c2302b8ed6bc8a02d32e7af81c504e23bbd
Mohamedeid106 <112629352+Mohamedeid106@users.noreply.github.com>
2024-08-26 15:58:24 +0300

    Add the structure

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import *
+import sqlite3
+
+app = Flask(__name__)
+
+def create_database():
+    conn = sqlite3.connect('project.db')
+    db = conn.cursor()
+    db.execute('''CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, password TEXT)''')
+    conn.commit()
+    conn.close()
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/login', methods=["POST"])
+def login():
+    name = request.form.get('name')
+    password = request.form.get('password')
+    
+    conn = sqlite3.connect('project.db')
+    db = conn.cursor()
+    db.execute("INSERT INTO users (name, password) VALUES (?,?)", (name, password))
+    conn.commit()
+    conn.close()
+    return redirect('/homepage')
+
+@app.route('/homepage')
+def hello():
+    # registerants = db.execute("SELECT * FROM users")
+    return render_template('homepage.html')
+
+
+if __name__ == '__main__':
+    create_database()
+    app.run(debug=True)
